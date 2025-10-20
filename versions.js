866da2551282e00cd0831505abcc6417d6ff6e4b
elchunen_freund <elchunenfreund@gmail.com>
2025-10-19 23:45:33 -0400

    versions exercise

--- versions.js
+++ versions.js
@@ -1,3 +1,5 @@
+// problem
+
 // - Input: two version numbers in string representation, version1 and version2
 // - Output: one of the numbers from 1, 0, and -1; or null for invalid inputs
 
@@ -16,6 +18,8 @@
 //     - keep moving to the right until the result of the comparison is determined
 //     - if we get to the last number part of the two version numbers and they're equal,
 //       then the two version numbers are equal
+
+// examples/test cases
 
 // - elements that are numbers should be multiplied by 2
 // - elements that are strings should be repeated twice via concatenation
@@ -40,7 +44,60 @@
 // - if the argument is an object, treat it as an array of its property values
 // - all other kinds of inputs are invalid, and should return the string 'Invalid input'
 
+// data structure
 
-function compareVersions(version1, version2) {
-  // ...
+//arrays
+
+// algorithm
+
+// - return null if the inputs contain any characters other than digits and dots
+// - split the input numbers into parts as arrays of integers
+// - loop through the two version numbers' parts
+//   - for each step, access one part from each version number
+//   - if one version number runs out of parts, use 0
+//   - compare the two parts
+//     - if part1 < part2
+//       - return -1
+//     - if part1 > part2
+//       - return 1
+//     - if part1 === part2
+//       - we move to the next pair of parts
+// - when we reach the end of the loop, return 0
+
+
+
+function compareVersions(versionA, versionB) {
+  let validChars = /^[0-9]+(\.[0-9]+)*$/;
+
+  if (!validChars.test(versionA) || !validChars.test(versionB)) {
+    return null;
+  }
+
+  let aParts = versionA.split('.').map(Number);
+  let bParts = versionB.split('.').map(Number);
+  let maxLength = Math.max(aParts.length, bParts.length);
+
+  for (let i = 0; i < maxLength; i += 1) {
+    let aValue = aParts[i] || 0;
+    let bValue = bParts[i] || 0;
+
+    if (aValue > bValue) {
+      return 1;
+    } else if (aValue < bValue) {
+      return -1;
+    }
+  }
+
+  return 0;
 }
+
+console.log(compareVersions('1', '1'));            // 0
+console.log(compareVersions('1.1', '1.0'));        // 1
+console.log(compareVersions('2.3.4', '2.3.5'));    // -1
+console.log(compareVersions('1.a', '1'));          // null
+console.log(compareVersions('.1', '1'));           // null
+console.log(compareVersions('1.', '2'));           // null
+console.log(compareVersions('1..0', '2.0'));       // null
+console.log(compareVersions('1.0', '1.0.0'));      // 0
+console.log(compareVersions('1.0.0', '1.1'));      // -1
+console.log(compareVersions('1.0', '1.0.5'));      // -1
